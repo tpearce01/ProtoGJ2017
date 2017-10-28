871100658f06be3086f796c68de8ea65b79e5a5a
hypergressive <acrotty@uci.edu>
2017-10-27 21:39:32 -0700

    design doc todo list

--- GJ2017/Assets/Scripts/Floating_Obstacles.cs
+++ GJ2017/Assets/Scripts/Floating_Obstacles.cs
@@ -7,9 +7,15 @@ public class Floating_Obstacles : MonoBehaviour {
 
     public float speed = 5f;
     Vector2 ran;
-    Vector3 direction;
+
+    GameObject camera;
+    float destory_distance = 30f;
     // Use this for initialization
     void Start () {
+
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        //start floating at a random direction and spinning randomly
         ran.x = Random.Range(-1, 1)*speed;
         ran.y = Random.Range(-1, 1)*speed;
         this.GetComponent<Rigidbody2D>().AddForce(ran);
@@ -18,18 +24,31 @@ public class Floating_Obstacles : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //hide the obstacle for now
         this.GetComponent<SpriteRenderer>().enabled = false;
       
+        //stop the obstacle from moving
         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-//screenshake
+        
+        //something screenshake
+        //something screenshake
 
-        this.GetComponent<EdgeCollider2D>().enabled = false;
+
+        //shut the collider to prevent exploding effect twice
+        this.GetComponent<PolygonCollider2D>().enabled = false;
+
+        //destory object
         this.GetComponent<ParticleSystem>().Play();
        
         Destroy(this.gameObject,2);
     }   
     // Update is called once per frame
     void Update () {
+
+        if (Vector3.Distance(this.transform.position, camera.transform.position) >= destory_distance) 
+        {
+            Destroy(this.gameObject);
+        }
         
     }
 }
