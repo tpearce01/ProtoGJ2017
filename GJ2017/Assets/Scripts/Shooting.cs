c2daa87128602016c59d179252fc0a6b10b24e2a
TinaFeng <xuanfeng1996@gmail.com>
2017-10-28 13:09:03 -0700

    machine gun with flame bullets :D

---
+++ GJ2017/Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    Vector3 target;
+    public GameObject bullet;
+    public float speed = 3f;
+    
+	// Use this for initialization
+	void Start () {
+
+        target = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            //...setting shoot direction
+            Vector3 shootDirection;
+            shootDirection = Input.mousePosition;
+            shootDirection.z = 0.0f;
+            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+            shootDirection = shootDirection - transform.position;
+            //...instantiating the rocket
+            Rigidbody2D bulletInstance = Instantiate(bullet.GetComponent<Rigidbody2D>(), transform.position, Quaternion.Euler(new Vector3(0, 0, shootDirection.z))) as Rigidbody2D;
+            
+            bulletInstance.velocity = new Vector2(shootDirection.x * speed, shootDirection.y * speed);
+
+        }
+
+
+    }
+
+
+}
---
+++ GJ2017/Assets/Scripts/bullet_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_script : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Destroy(this.gameObject, 2f);   
+    }
+}
