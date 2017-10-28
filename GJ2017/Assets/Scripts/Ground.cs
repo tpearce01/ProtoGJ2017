6f36b15ccae84031a2d74b931bc477753f132948
Tyler Pearce <pearcetyler@yahoo.com>
2017-10-27 21:40:26 -0700

    Added Ground Collision
    - Ground follows player
    - Added collider

---
+++ GJ2017/Assets/Scripts/Ground.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector2 (Rocket.r.gameObject.transform.position.x, gameObject.transform.position.y);
+	}
+}
