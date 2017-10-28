7a7ad886512b5a5f935c2bd0d36c3380803e4dc5
TinaFeng <xuanfeng1996@gmail.com>
2017-10-27 19:06:40 -0700

    Particle_System_and_meteor

---
+++ GJ2017/Assets/Floating_Obstacles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating_Obstacles : MonoBehaviour {
+
+
+    public float speed = 5f;
+    Vector2 ran;
+    Vector3 direction;
+    // Use this for initialization
+    void Start () {
+        ran.x = Random.Range(-1, 1)*speed;
+        ran.y = Random.Range(-1, 1)*speed;
+        this.GetComponent<Rigidbody2D>().AddForce(ran);
+        this.GetComponent<Rigidbody2D>().AddTorque(Random.Range(-50,50));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
