82225aedc78e8f44c3a01532918cfae5191fb0fa
Tyler Pearce <pearcetyler@yahoo.com>
2017-10-27 19:00:29 -0700

    Simple Rocket Script
    Launches

---
+++ GJ2017/Assets/Scripts/HUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+	[SerializeField] Text metrics;
+	[SerializeField] Rocket r;
+	[SerializeField] Rigidbody2D rb;
+
+	void Update(){
+		metrics.text = "Velocity: " + rb.velocity + "\n" +
+		"Height: " + r.transform.position.y;
+	}
+}
---
+++ GJ2017/Assets/Scripts/Rocket.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	bool hasLaunched = false;
+	[SerializeField] Rigidbody2D rocket;
+	[SerializeField] GameObject fuelEffect;
+
+	[SerializeField]int maxFuel;
+	[SerializeField]float currentFuel;
+	[SerializeField]int enginePower;
+	[SerializeField]int launchPower;
+	[SerializeField]int shield;
+	[SerializeField]float drag;
+	[SerializeField]int maxAmmo;
+	[SerializeField]int currentAmmo;
+	[SerializeField]Weapon weapon;
+
+	void Start(){
+		Initialize ();
+	}
+
+	void Update(){
+		if (!hasLaunched && Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Launched");
+			hasLaunched = true;
+			rocket.AddForce(new Vector2(0, launchPower));
+			fuelEffect.SetActive (true);
+			fuelEffect.GetComponent<ParticleSystem> ().Play ();
+		}
+	}
+
+	void FixedUpdate(){
+		if (hasLaunched && currentFuel > 0) {
+			Debug.Log ("Fueling");
+			currentFuel -= 0.1f;
+			rocket.AddForce (new Vector2(0, enginePower));
+			if (currentFuel <= 0) {
+				fuelEffect.GetComponent<ParticleSystem>().Stop();
+			}
+		}
+	}
+
+	public void Initialize(){
+		maxFuel = 10;
+		currentFuel = 10;
+		enginePower = 10;
+		launchPower = 1000;
+		shield = 10;
+		drag = 10;
+		maxAmmo = 10;
+		currentAmmo = 10;
+		weapon = Weapon.Basic;
+	}
+}
+
+public enum Weapon{
+	Basic = 0
+}
