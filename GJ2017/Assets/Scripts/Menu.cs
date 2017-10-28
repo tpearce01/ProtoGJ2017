0beab1ef272cb11276dbe43f32444d1904693b80
Tyler Pearce <pearcetyler@yahoo.com>
2017-10-27 20:52:37 -0700

    Added WIP menus
    ** Broken, emergency push to access other branch

---
+++ GJ2017/Assets/Scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	string sceneName = "tylers_scene";
+	string mainMenuScene = "main_menu";
+
+	[SerializeField] List<GameObject> menus = new List<GameObject>();
+
+	public void Resume(){
+		CloseAll ();
+		Time.timeScale = 1;
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void Shop(){
+		CloseAll ();
+		Pause ();
+		menus [(int)MenuPanel.ShopMenu].SetActive (true);
+	}
+
+	public void PauseMenu(){
+		CloseAll ();
+		Pause ();
+		menus [(int)MenuPanel.PauseMenu].SetActive (true);
+	}
+
+	public void IntermissionMenu(){
+		Pause ();
+		menus [(int)MenuPanel.IntermissionMenu].SetActive (true);
+	}
+
+	void CloseAll(){
+		foreach (GameObject go in menus) {
+			go.SetActive (false);
+		}
+	}
+
+	public void Pause(){
+		Time.timeScale = 0;
+	}
+
+	public void ExitToMainMenu(){
+		SceneManager.LoadScene (mainMenuScene);
+	}
+}
+
+public enum MenuPanel{
+	PauseMenu = 0,
+	ShopMenu = 1,
+	IntermissionMenu = 2
+}
