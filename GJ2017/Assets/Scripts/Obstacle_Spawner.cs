edc049d387a73ccfebe07c706e68e2b8d902458e
TinaFeng <xuanfeng1996@gmail.com>
2017-10-27 20:52:55 -0700

    obstacle_spawner_completer

---
+++ GJ2017/Assets/Scripts/Obstacle_Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Spawner : MonoBehaviour {
+
+    public GameObject Meteor;
+    public int amount;
+    Vector3 new_position;
+
+    int counter;
+    public float range = 40f;
+
+    GameObject[] obstacles;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        obstacles = GameObject.FindGameObjectsWithTag("obstacle");
+            
+        if (obstacles.Length <= amount)
+        {
+            new_position = new Vector3(Random.Range(this.transform.position.x - range, this.transform.position.x + range),
+                                        Random.Range(this.transform.position.y - range, this.transform.position.y + range), 0);
+
+            Instantiate(Meteor, new_position, Quaternion.identity);
+        }  
+	}
+}
