367e6d4e7f4a103510bbd139b297dbb0cb686a53
Tyler Pearce <pearcetyler@yahoo.com>
2017-10-28 21:06:35 -0700

    Fixed HUD
    - Fixed angular velocity

--- GJ2017/Assets/Scripts/Rocket.cs
+++ GJ2017/Assets/Scripts/Rocket.cs
@@ -231,6 +231,7 @@ public class Rocket : MonoBehaviour {
 		timeToRoundEnd = timeToRoundEndMaster;
 		gameObject.transform.rotation = baseRotation;
 		starEffect.gameObject.SetActive (false);
+		rocket.angularVelocity = 0;
 	}
 
 	void Movement(){
