c50b95a5a3bd8a481de68c4e06f25dc254ca693f
TinaFeng <xuanfeng1996@gmail.com>
2017-10-29 20:29:58 -0700

    my thing and crap meta

---
+++ GJ2017/Assets/Glass_Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glass_Wall : MonoBehaviour {
+
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            anim.SetBool("Shatter", true);
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            
+        }
+
+    }
+    // Update is called once per frame
+
+    void Update () {
+		
+	}
+}
